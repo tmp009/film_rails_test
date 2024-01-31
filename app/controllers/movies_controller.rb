8edc1201e771455029f13b682006518ff58fb756
tmp009 <128413103+tmp009@users.noreply.github.com>
2024-01-31 14:09:37 +0200

    add language id parameter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -65,6 +65,6 @@ class MoviesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def movie_params
-      params.require(:movie).permit(:film_id, :title, :description)
+      params.require(:movie).permit(:film_id, :title, :description, :language_id)
     end
 end
